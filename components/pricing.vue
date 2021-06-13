9d433679434ee6bb2a8dcc6e7fbbfac30eada5cd
stang464 <phoatipong@gmail.com>
2021-06-13 15:30:03 +0700

    ...

--- components/pricing.vue
+++ components/pricing.vue
@@ -1,1 +1,40 @@
-vue
+<template>
+  <div class="app">
+    <v-container>
+      <v-row class="mt-5">
+        <v-col cols="12" sm="12">
+          <h1 class="text-center">ตารางเปรียบเทียบแพ็กเกจ</h1>
+        </v-col>
+      </v-row>
+      <v-row class="mt-5">
+        <v-col cols="4" xs="12">
+          <v-card class="rounded-xl">
+            <v-row>
+              <v-col cols="12" class="text-center mt-5">
+                <h3>Connect</h3>
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-col>
+        <v-col cols="4"></v-col>
+        <v-col cols="4"></v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.card-pricing {
+  height: 971px;
+  left: calc(50% - 400px / 2 - 480px);
+  top: -37.28px;
+
+  background: #ffffff;
+  box-shadow: 0px 18px 50px -15px #060f28;
+  border-radius: 30px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,17 +3,20 @@
     <Navbar />
     <Silde />
     <spot />
+    <pricing />
   </v-app>
 </template>
 <script>
 import Navbar from '~/components/navbar.vue'
 import Silde from '~/components/silde.vue'
 import spot from '~/components/spot.vue'
+import pricing from '~/components/pricing'
 export default {
   components: {
     spot,
     Navbar,
     Silde,
+    pricing,
   },
 }
 </script>
