9cb4fa610b55b628a114ce97fbe375b12190845a
sakon2856 <68525340+sakonwantaeng2856@users.noreply.github.com>
2021-06-13 14:51:58 +0700

    upanddown

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,17 +2,22 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true,
+    node: true
   },
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: 'babel-eslint'
   },
   extends: [
-    '@nuxtjs',
+    'prettier',
+    'prettier/vue',
     'plugin:prettier/recommended',
-    'plugin:nuxt/recommended',
+    '@nuxtjs',
+    'plugin:nuxt/recommended'
   ],
-  plugins: [],
+  plugins: [
+    'prettier'
+  ],
   // add your custom rules here
-  rules: {},
+  rules: {
+  }
 }
--- components/silde.vue
+++ components/silde.vue
@@ -3,27 +3,27 @@
     <v-container fluid>
       <v-row class="bg">
         <v-col cols="6" class="bg-1">
-          <div class="mr-5 mt-4">
-            <v-col class="mt-4">
+          <div class="mr-5 mt-5">
+            <v-col class="mt-5">
               <h1 class="text1 float-right">ระบบเว็บไซต์หน่วยงานสำเร็จรูป</h1>
             </v-col>
             <v-col class="mt-4">
-              <h5 class="text1 float-right">
+              <h3 class="text2 float-right">
                 สร้างไซต์หน่วยงานท่านได้ตัวเอง ง่าย เร็ว สวยงาม
-              </h5>
+              </h3>
             </v-col>
             <v-col class="mt-2">
-              <h5 class="text1 float-right">
+              <h3 class="text3 float-right">
                 MANAGE YOUR ORGANIZATION , EASY , FAST , BEAUTIFUL
-              </h5>
+              </h3>
             </v-col>
             <v-col class="float-right">
               <v-btn class="float-right" color="success">ทดลองใช้งาน</v-btn>
             </v-col>
             <v-col class="day">
-              <h5 class="text1 float-right">
+              <h3 class="text4 float-right">
                 * สมัครใช้งานวันนี้ทดลองใช้ฟรี 30วัน
-              </h5>
+              </h3>
             </v-col>
           </div>
         </v-col>
@@ -45,18 +45,5 @@ export default {}
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Kanit:wght@300&display=swap');
-.app {
-  font-family: 'Kanit', sans-serif;
-}
-.bg {
-  background-color: #f1f1f1;
-}
-.bg-1 {
-  background-color: #e0e0e0;
-  border-radius: 0px 84px 84px 0px;
-}
-.day {
-  color: rgb(255, 0, 0);
-}
+@import '../assets/slide.css';
 </style>
